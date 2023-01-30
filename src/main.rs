902d3516762a358da2bf7e3074a125d4cc645ffc
Kyle Chamberlin <Kyle@Chamberl.in>
2023-01-29 20:40:33 -0500

    basic templating and directory

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-use std::{path::PathBuf, fs};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
 use lazy_static::lazy_static;
-use tera::{Tera, Context};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use shellexpand;
+use tera::{Context, Tera};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Args {
-    #[arg(long, env, default_value="~/.zsh_history")]
+    #[arg(long, env, default_value = "~/.zsh_history")]
     histfile: PathBuf,
+
+    #[arg(long, env, default_value = "~/.funky/")]
+    funky_dir: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,15 +34,42 @@ fn main() {
 
     let _args = Args::parse();
 
-    let histfile_contents = fs::read_to_string(_args.histfile.as_path()).expect("Histfile should exist");
+    let histfile_contents =
+        fs::read_to_string(_args.histfile.as_path()).expect("Histfile should exist");
 
     let last_entry = histfile_contents.lines().rev().nth(1).unwrap();
 
-    let func = Func{name: "cmd".to_string(), arguments: vec!["arg1".to_string(), "arg2".to_string()], command: last_entry.to_string()};
+    let func = Func {
+        name: "cmd".to_string(),
+        arguments: vec!["arg1".to_string(), "arg2".to_string()],
+        command: last_entry.to_string(),
+    };
 
-    let function_out = TERA.render("functions/zsh", &Context::from_serialize(func).expect("failed to serialize context")).expect("failed to render template into string");
-    
-    println!("{function_out}");
+    let _function_out = TERA
+        .render(
+            "functions/zsh",
+            &Context::from_serialize(func).expect("failed to serialize context"),
+        )
+        .expect("failed to render template into string");
+
+    let funky_dir = get_dir(_args.funky_dir);
+
+    dbg!(funky_dir);
+}
+
+fn get_dir(path: String) -> PathBuf {
+    let expanded_path = shellexpand::full(&path).expect("Failed to expand path.");
+    let dir = PathBuf::from(expanded_path.to_string());
+
+    if dir.exists() {
+        if !dir.is_dir() {
+            panic!("provided path is not a directory.");
+        }
+    } else {
+        fs::create_dir_all(&dir).expect("unable to create dir");
+    };
+
+    return dir;
 }
 
 lazy_static! {
