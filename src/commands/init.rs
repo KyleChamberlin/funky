b1e7491a6fb87929d845297b78c74009926aa780
Kyle Chamberlin <Kyle@Chamberl.in>
2023-02-03 22:11:41 -0500

    add init command

---
+++ src/commands/init.rs
@@ -0,0 +1,30 @@
+use clap::{Parser, ValueEnum};
+
+use super::new::FunctionSource;
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    #[arg()]
+    name: String,
+
+    #[arg(value_enum, default_value = "zsh")]
+    shell: Shell,
+
+    /// if another function is found in FUNKY_DIR with the same NAME,
+    /// overwrite the contents of that function without prompting.
+    #[arg(long)]
+    overwrite: bool,
+
+    /// The command you wish to make funky.
+    /// If your command includes shell interpreted glyphs you will need to either
+    /// escape them or quote your command to stop shell interpretation.
+    #[arg(name = "vargs", last = true)]
+    function: Option<Vec<String>>,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+pub enum Shell {
+    Zsh,
+    Bash,
+    Powershell,
+}
