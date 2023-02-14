d5db988a49ceab251e0dd028b4137ab3f14aa8f3
Kyle Chamberlin <Kyle@Chamberl.in>
2023-02-14 14:06:11 -0500

    formatting

--- src/file.rs
+++ src/file.rs
@@ -2,31 +2,31 @@ use color_eyre::{eyre::eyre, Result};
 use std::{fs, path::PathBuf};
 
 pub fn get_dir(path: String) -> Result<PathBuf> {
-    let expanded_path = shellexpand::full(&path)?;
-    let dir = PathBuf::from(expanded_path.to_string());
+  let expanded_path = shellexpand::full(&path)?;
+  let dir = PathBuf::from(expanded_path.to_string());
 
-    if dir.exists() {
-        if !dir.is_dir() {
-            return Err(eyre!("provided directory {path} is not a directory"));
-        }
-    } else {
-        fs::create_dir_all(&dir)?;
-    };
+  if dir.exists() {
+    if !dir.is_dir() {
+      return Err(eyre!("provided directory {path} is not a directory"));
+    }
+  } else {
+    fs::create_dir_all(&dir)?;
+  };
 
-    Ok(dir)
+  Ok(dir)
 }
 
 pub fn get_file(path: String) -> Result<PathBuf> {
-    let expanded_path = shellexpand::full(&path)?;
-    let file = PathBuf::from(expanded_path.to_string());
+  let expanded_path = shellexpand::full(&path)?;
+  let file = PathBuf::from(expanded_path.to_string());
 
-    if file.exists() {
-        if file.is_dir() {
-            return Err(eyre!("provided file {path} is a directory"));
-        }
-    } else {
-        return Err(eyre!("file {path} does not exist"));
-    };
+  if file.exists() {
+    if file.is_dir() {
+      return Err(eyre!("provided file {path} is a directory"));
+    }
+  } else {
+    return Err(eyre!("file {path} does not exist"));
+  };
 
-    Ok(file)
+  Ok(file)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ pub mod config;
 pub mod file;
 
 lazy_static! {
-    pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("template/**/*") {
-            Ok(t) => t,
-            Err(e) => {
-                println!("Parsing error(s): {e}");
-                ::std::process::exit(127);
-            }
-        };
-        tera
+  pub static ref TEMPLATES: Tera = {
+    let tera = match Tera::new("template/**/*") {
+      Ok(t) => t,
+      Err(e) => {
+        println!("Parsing error(s): {e}");
+        ::std::process::exit(127);
+      }
     };
+    tera
+  };
 }
